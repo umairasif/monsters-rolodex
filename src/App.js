dfafc29ec7e0719b454aae1566414d12f39db84d
Umair Asif <umair.asif@manafatech.com>
2024-10-16 10:49:34 +0500

    monsters rolodex - component state

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Umair',
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi Umair
+            Hi {this.state.name}
           </p>
           <button>Change Name</button>
         </header>
