49997d8f595eb03ad530f65c89b4aa36f4e408aa
Umair Asif <umair.asif@manafatech.com>
2024-10-16 15:33:55 +0500

    set state callback

--- src/App.js
+++ src/App.js
@@ -8,18 +8,27 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Umair',
-    }
+      name: "Umair",
+    };
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name}
-          </p>
-          <button onClick={() => {this.setState({name: 'Umair Asif'})}}>Change Name</button>
+          <p>Hi {this.state.name}</p>
+          <button
+            onClick={() => {
+              this.setState(
+                () => {return {name: 'Umair Asif'};}, 
+                () => {
+                  console.log(this.state);
+                }
+                );
+            }}
+          >
+            Change Name
+          </button>
         </header>
       </div>
     );
